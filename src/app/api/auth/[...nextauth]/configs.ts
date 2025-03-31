9a703477a8497b3dae1b89c09fcd9ba4df27531e
Jawad Sher <jawadsher04@gmail.com>
2025-03-31 12:58:02 +0500

    Change session strategy to database and enhance Redis user data deletion on sign-out

--- src/app/api/auth/[...nextauth]/configs.ts
+++ src/app/api/auth/[...nextauth]/configs.ts
@@ -246,7 +246,7 @@ export async function initAuthConfigs() {
       error: "/sign-in",
     },
     session: {
-      strategy: "jwt",
+      strategy: "database",
       maxAge: 30 * 24 * 60 * 60,
     },
     secret: process.env.NEXTAUTH_SECRET,
--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,19 +1,22 @@
-import { signOut } from "@/app/api/auth/[...nextauth]/configs"
+import { signOut } from "@/app/api/auth/[...nextauth]/configs";
 import { connectRedis } from "@/lib/redis";
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest){
-    const token = await getToken({ req });
-    if(token && token._id){
-        try{
-            const redis = await connectRedis();
-            await redis.del(`app:user:${token._id}`);
-        }catch(error){
-            console.error("Error deleting user data from redis: ", error);
-        }
+export async function POST(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  if (token && token._id) {
+    try {
+      const redis = await connectRedis();
+      const userKeys = await redis.keys(`user:*${token._id}*`);
+      for (const key of userKeys) {
+        await redis.del(key);
+      }
+    } catch (error) {
+      console.error("Error deleting user data from redis: ", error);
     }
+  }
 
-    await signOut();
-    return NextResponse.redirect(new URL('/sign-in', req.url));
+  await signOut();
+  return NextResponse.redirect(new URL("/sign-in", req.url));
 }
