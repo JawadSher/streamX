9ca41df400e6adc1bce2760c19c7e317b6b8c34b
Jawad Sher <jawadsher04@gmail.com>
2025-03-31 12:30:44 +0500

    Add Redis cleanup for user data on sign-out; handle token retrieval and error logging

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,7 +1,19 @@
 import { signOut } from "@/app/api/auth/[...nextauth]/configs"
+import { connectRedis } from "@/lib/redis";
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest){
+    const token = await getToken({ req });
+    if(token && token._id){
+        try{
+            const redis = await connectRedis();
+            await redis.del(`app:user:${token._id}`);
+        }catch(error){
+            console.error("Error deleting user data from redis: ", error);
+        }
+    }
+
     await signOut();
     return NextResponse.redirect(new URL('/sign-in', req.url));
 }
