0d50752b02d098689e618f1c229cc7bda40f3331
Jawad Sher <jawadsher04@gmail.com>
2025-04-19 10:19:31 +0500

    feat: update UserAvatar component; improve accessibility by setting dynamic alt text based on user's full name

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -9,7 +9,7 @@ export async function UserAvatar({ avatarURL, fullName }: Props) {
 
   return (
     <div className="rounded-full border-2 border-zinc-400 p-1">
-      <Image src={avatarURL} width={100} height={100} alt="logo" quality={75} className="rounded-full"/>
+      <Image src={avatarURL} width={100} height={100} alt={`${fullName?.split(" ")[0][0]}`} quality={75} className="rounded-full"/>
     </div>
   );
 }
