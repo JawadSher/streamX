7114682c47d748ef0516031051002b8cc3c347af
Jawad Sher <jawadsher04@gmail.com>
2025-04-28 08:37:37 +0500

    fix: remove unnecessary display parameter from Material Symbols font link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
         />
         <script
           type="text/javascript"
